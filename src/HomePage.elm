b3a1db85b162bf95c4b024018ef7066f8a7810e1
Ashton Gabrielsen <ashton.gabrielsen@drip.com>
2020-11-10 18:58:21 -0700

    init mockData as string from local file

--- src/HomePage.elm
+++ src/HomePage.elm
@@ -1,7 +1,9 @@
-module HomePage exposing (main)
+module HomePage exposing (main, Model)
 
+import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import ScoresTable exposing (showScoresTable)
 
 view model =
     div []
@@ -13,16 +15,32 @@ view model =
         , div [] [
             p [] [ text "A description will go here." ]
         ]
-        , table [ id "class-scores" ] [
-            tr [] [
-                td [] [ text "Ashton" ]
-                , td [] [ text "Gabrielsen" ]
-                , td [] [ text "100" ]
-                , td [] [ text "80" ]
-                , td [] [ text "80%" ]
-            ]
+        , showScoresTable model.mockData
         ]
-    ]
 
+main : Program String Model Msg
 main =
-    view "dummy model"
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+type alias Model = { mockData : String }
+
+init : String -> ( Model, Cmd Msg )
+init mockData =
+    ( { mockData = mockData}
+    , Cmd.none
+    )
+
+type Msg = NoOp
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update _ model =
+        (model, Cmd.none )
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
--- src/ScoresTable.elm
+++ src/ScoresTable.elm
@@ -1,4 +1,20 @@
-module ScoresTable exposing (..)
+module ScoresTable exposing (showScoresTable)
 
-parseScoresCsv =
-    2 + 2
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+showScoresTable : String -> Html msg
+showScoresTable mockdata =
+    div []
+        [ table [ id "class-scores" ]
+            [ tr []
+                [ td [] [ text "Ashton" ]
+                  , td [] [ text "Gabrielsen" ]
+                  , td [] [ text "100" ]
+                  , td [] [ text "80" ]
+                  , td [] [ text "80%" ]
+                ]
+            ]
+        , div []
+            [ text mockdata ]
+        ]
